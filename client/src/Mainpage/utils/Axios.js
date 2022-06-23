0f5def86e7dfce7567fc266375ea1f89279acb13
Jeremy <lf2lf2kk@gmail.com>
2022-06-23 13:20:09 +1000

    change cors options, clear all manual setting to cors

--- client/src/Mainpage/utils/Axios.js
+++ client/src/Mainpage/utils/Axios.js
@@ -1,7 +1,7 @@
 import _axios from "axios";
 
 const Axios = () => {
-    const instance = _axios.create({ baseURL: "http://localhost:5000/" });
+    const instance = _axios.create({ baseURL: "https://broccolimedia.herokuapp.com/" });
     return instance;
 }
 
