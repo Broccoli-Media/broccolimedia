6f34bd40a29eea6815bdb6cd0511141b785d5938
Jeremy <lf2lf2kk@gmail.com>
2022-06-25 05:21:51 +1000

    add proxy to frontend

--- server/Server.js
+++ server/Server.js
@@ -16,15 +16,6 @@ const STATUS_500 = 500;
 const app = express();
 dotenv.config();
 // Middleware
-var corsOptions = {
-	'origin': "https://broccolimedia.net",
-	'allowedHeaders': "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, x-content-type-options",
-	'exposedHeaders': "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, x-content-type-options",
-	'methods': "GET,HEAD,PUT,UPDATE,POST,DELETE",
-	'credential': true,
-	'preflightContinue': false,
-	'maxage': 31536000
-}
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
@@ -36,27 +27,7 @@ mongoose
 	.catch((err) => console.log(err));
 
 mongoose.connection.on("disconnected", () => { console.log("Fail to connect MongoDB"); });
-
-// Socket
-// const server = http.createServer(app);
-// const io = new Server(server);
-// io.of("/api/socket").on("connection", (socket) => {
-// 	console.log("socket.io: User connected: ", socket.id);
-// 	socket.on("disconnect", () => {
-// 		console.log("socket.io: User disconnected: ", socket.id);
-// 	});
-// });
 // Routes
-function corsSet (req, res, next){
-	res.header("Access-Control-Allow-Origin", "https://broccolimedia.net");
-	res.header("Access-Control-Allow-Headers","Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, x-content-type-options");
-	res.header("Access-Control-Exposed-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, x-content-type-options");
-	res.header("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, UPDATE");
-	res.header("Access-Control-Allow-Credentials", true);
-	res.header("Access-Control-Max-Age", 31536000);
-	next();
-}
-// app.options('*', cors());
 app.use("/auth", authRoute);
 app.use("/user", userRoute);
 app.get('/test', testRoute);
@@ -70,5 +41,4 @@ app.use((err, req, res, next) => {
 		stack: err.stack,
 	});
 });
-// const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => { console.log(`Listening to ${PORT}`); });
